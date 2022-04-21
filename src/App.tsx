12932f9d1327adc370132e5ef4c5d570ac92552f
Aziz Akgul <azizakgul@gmail.com>
2022-04-21 09:31:13 +0300

    Web getDonations from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState, useRef}  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from '@mui/material';
+import * as Axios from "axios"
 
 function App() {
+
+  const axios = Axios.default
+
+
+  useEffect(() => {
+        
+    
+
+  }, []);
+
+  async function getDonations(email:string):Promise<GetDonations|undefined>{
+
+    const lambdaUrl = "https://g3kfr51f9h.execute-api.us-east-1.amazonaws.com/staging/donations/"
+      
+    try {
+      const response = await axios.get(`${lambdaUrl}${email}`);
+      return response as GetDonations
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+      return
+    }
+  }
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +39,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button>Make a donation</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +51,19 @@ function App() {
       </header>
     </div>
   );
+}
+
+
+interface Donation {
+  email  : string
+  amount : number
+
+}
+
+interface GetDonations {
+  error?  : string
+  amount? : number
+  times?  : number
 }
 
 export default App;
